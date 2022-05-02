7e6d075c94ef715db7420d348765f95cc05a4385
Albert <ygyhh123@gmail.com>
2022-05-02 15:11:40 +0800

    build_1_1

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-14x8q)gy)8l=4a=@ygt=h&uhe%mtyatf#o#63_-&#$5733t+a_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
